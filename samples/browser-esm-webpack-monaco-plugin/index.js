20d7e2c893c7aebff1f15d39712f8f996a2f2fc9
Garrett Campbell <gcampbell@microsoft.com>
2022-03-30 16:27:46 -0700

    push to fork

--- samples/browser-esm-webpack-monaco-plugin/index.js
+++ samples/browser-esm-webpack-monaco-plugin/index.js
@@ -9,8 +9,47 @@ import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 	document.body.appendChild(div);
 })();
 
-monaco.editor.create(document.getElementById('root'), {
-	value: `const foo = () => 0;`,
-	language: 'javascript',
-	theme: 'vs-dark'
+var editor = monaco.editor.create(document.getElementById('root'), {
+	value: [''].join('\n'),
+	language: 'text',
+	wordWrap: 'off',
+	lineNumbers: 'on',
+	glyphMargin: false,
+	lineDecorationsWidth: 20,
+	rulers: [],
+	folding: false,
+	scrollBeyondLastLine: false,
+	renderLineHighlight: 'none',
+	renderValidationDecorations: 'editable',
+	padding: undefined,
+	readOnly: true,
+	domReadOnly: true,
+	fixedOverflowWidgets: true,
+	theme: 'vs-dark',
+	contextmenu: false // this disables the ability to show the context menu
 });
+
+// this disables the monaco keybinding, which disables the monaco search box, which might not 
+// be VS themeable.
+editor._standaloneKeybindingService.addDynamicKeybinding("-actions.find", null, () => {});
+
+// disables the F1 command pallete
+editor.addCommand(monaco.KeyCode.F1, () => {});
+
+const method = () => {
+	const lineCount = editor.getModel().getLineCount();
+	const lastLineLength = editor.getModel().getLineMaxColumn(lineCount);
+
+	const range = new monaco.Range(
+		lineCount,
+		lastLineLength,
+		lineCount,
+		lastLineLength
+	);
+
+	const oneNewlineText = "new line: \n";
+
+	editor.getModel().applyEdits([{text: oneNewlineText, range: range}]);
+};
+
+setInterval(method, .01);
